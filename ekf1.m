f46c7a0a757fe9f2aaaa6923cd91902158b4b995
yuwwang123 <57539328+yuwwang123@users.noreply.github.com>
2020-05-22 20:12:30 -0400

    Update ekf1.m

--- ekf1.m
+++ ekf1.m
@@ -10,34 +10,19 @@ function [X, Z] = ekf1(sensor, vic, varargin)
 %          - id: 1xn ids of detected tags
 %          - p0, p1, p2, p3, p4: 2xn pixel position of center and
 %                                four corners of detected tags
-%            Y
-%            ^ P3 == P2
-%            | || P0 ||
-%            | P4 == P1
-%            o---------> X
+
 %   vic    - struct for storing vicon linear velocity in world frame and
 %            angular velocity in body frame, fields include
 %          - t: vicon timestamp
 %          - vel = [vx; vy; vz; wx; wy; wz]
-%   varargin - any variables you wish to pass into the function, could be
-%              a data structure to represent the map or camera parameters,
-%              your decision. But for the purpose of testing, since we don't
-%              know what inputs you will use, you have to specify them in
-%              init_script by doing
-%              ekf1_handle = ...
-%                  @(sensor, vic) ekf1(sensor, vic, your input arguments);
+
 %
 % OUTPUTS:
 % X - nx1 state of the quadrotor, n should be greater or equal to 6
 %     the state should be in the following order
 %     [x; y; z; qw; qx; qy; qz; other states you use]
 %     we will only take the first 7 rows of X
-% OPTIONAL OUTPUTS:
-% Z - mx1 measurement of your pose estimator, m shoulb be greater or equal to 7
-%     the measurement should be in the following order
-%     [x; y; z; qw; qx; qy; qz; other measurement you use]
-%     note that this output is optional, it's here in case you want to log your
-%     measurement
+
 
 persistent Myu Sigma t_prev
 
